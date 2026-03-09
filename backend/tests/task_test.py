dc33349faf648686d7c0a4d40f7baa10e9af4d25
Nikita <zzzatoox@mail.ru>
2026-03-09 21:28:06 +0500

    feat: enchance tasks tests

--- backend/tests/task_test.py
+++ backend/tests/task_test.py
@@ -11,16 +11,21 @@ TEST_USER_DATA = {
     "patronymic": "Test",
 }
 
+TEST_USER_DATA2 = {
+    "login": "test2",
+    "email": "test2@mail.ru",
+    "password": "TestPass123!",
+    "password_confirm": "TestPass123!",
+    "last_name": "Test2",
+    "first_name": "User2",
+    "patronymic": "Test2",
+}
+
 TEST_TASK_DATA = {"title": "test", "owner_id": 1, "priority_id": 1, "status_id": 1}
 
 
 @pytest.mark.anyio
 async def test_get_all_tasks(client: AsyncClient):
-    tasks_response = await client.get("/tasks")
-    assert tasks_response.status_code == 200
-    tasks_data = tasks_response.json()
-    assert len(tasks_data) == 0
-
     register_response = await client.post(
         "/users",
         json=TEST_USER_DATA,
@@ -39,8 +44,13 @@ async def test_get_all_tasks(client: AsyncClient):
     assert login_response.status_code == 200
 
     token_data = login_response.json()
-
     assert "access_token" in token_data
+    headers = {"Authorization": f"Bearer {token_data['access_token']}"}
+
+    tasks_response = await client.get("/tasks", headers=headers)
+    assert tasks_response.status_code == 200
+    tasks_data = tasks_response.json()
+    assert len(tasks_data) == 0
 
     priority_response = await client.post("/priorities", json={"title": "Низкий"})
     assert priority_response.status_code == 200
@@ -49,9 +59,11 @@ async def test_get_all_tasks(client: AsyncClient):
     assert status_response.status_code == 200
 
     task_response = await client.post("/tasks", json=TEST_TASK_DATA)
+    assert task_response.status_code == 401
+    task_response = await client.post("/tasks", headers=headers, json=TEST_TASK_DATA)
     assert task_response.status_code == 200
 
-    tasks_response = await client.get("/tasks")
+    tasks_response = await client.get("/tasks", headers=headers)
     assert tasks_response.status_code == 200
 
     tasks_data = tasks_response.json()
@@ -81,6 +93,8 @@ async def test_create_task(client: AsyncClient):
 
     assert "access_token" in token_data
 
+    headers = {"Authorization": f"Bearer {token_data['access_token']}"}
+
     priority_response = await client.post("/priorities", json={"title": "Низкий"})
     assert priority_response.status_code == 200
 
@@ -88,6 +102,8 @@ async def test_create_task(client: AsyncClient):
     assert status_response.status_code == 200
 
     task_response = await client.post("/tasks", json=TEST_TASK_DATA)
+    assert task_response.status_code == 401
+    task_response = await client.post("/tasks", headers=headers, json=TEST_TASK_DATA)
     assert task_response.status_code == 200
 
     task_data = task_response.json()
@@ -119,6 +135,29 @@ async def test_get_task_by_id(client: AsyncClient):
     token_data = login_response.json()
 
     assert "access_token" in token_data
+    headers = {"Authorization": f"Bearer {token_data['access_token']}"}
+
+    register_response = await client.post(
+        "/users",
+        json=TEST_USER_DATA2,
+    )
+
+    assert register_response.status_code == 200
+
+    login_response = await client.post(
+        "/token",
+        data={
+            "username": TEST_USER_DATA2["login"],
+            "password": TEST_USER_DATA["password"],
+        },
+    )
+
+    assert login_response.status_code == 200
+
+    token_data = login_response.json()
+
+    assert "access_token" in token_data
+    headers2 = {"Authorization": f"Bearer {token_data['access_token']}"}
 
     priority_response = await client.post("/priorities", json={"title": "Низкий"})
     assert priority_response.status_code == 200
@@ -126,11 +165,15 @@ async def test_get_task_by_id(client: AsyncClient):
     status_response = await client.post("/statuses", json={"title": "Открыт"})
     assert status_response.status_code == 200
 
-    task_create_response = await client.post("/tasks", json=TEST_TASK_DATA)
+    task_create_response = await client.post(
+        "/tasks", headers=headers, json=TEST_TASK_DATA
+    )
     assert task_create_response.status_code == 200
     task_data = task_create_response.json()
 
-    task_response = await client.get(f"/tasks/{task_data['id']}")
+    task_response = await client.get(f"/tasks/{task_data['id']}", headers=headers2)
+    assert task_response.status_code == 403
+    task_response = await client.get(f"/tasks/{task_data['id']}", headers=headers)
     assert task_response.status_code == 200
 
     task_data2 = task_response.json()
@@ -159,6 +202,7 @@ async def test_update_task(client: AsyncClient):
     token_data = login_response.json()
 
     assert "access_token" in token_data
+    headers = {"Authorization": f"Bearer {token_data['access_token']}"}
 
     priority_response = await client.post("/priorities", json={"title": "Низкий"})
     assert priority_response.status_code == 200
@@ -166,13 +210,17 @@ async def test_update_task(client: AsyncClient):
     status_response = await client.post("/statuses", json={"title": "Открыт"})
     assert status_response.status_code == 200
 
-    task_create_response = await client.post("/tasks", json=TEST_TASK_DATA)
+    task_create_response = await client.post(
+        "/tasks", headers=headers, json=TEST_TASK_DATA
+    )
     assert task_create_response.status_code == 200
     task_data = task_create_response.json()
 
     task_data["title"] = "test2"
 
-    task_update_response = await client.put(f"/tasks/{task_data['id']}", json=task_data)
+    task_update_response = await client.put(
+        f"/tasks/{task_data['id']}", headers=headers, json=task_data
+    )
     assert task_update_response.status_code == 200
 
     updated_task_data = task_update_response.json()
@@ -201,6 +249,7 @@ async def test_delete_task(client: AsyncClient):
     token_data = login_response.json()
 
     assert "access_token" in token_data
+    headers = {"Authorization": f"Bearer {token_data['access_token']}"}
 
     priority_response = await client.post("/priorities", json={"title": "Низкий"})
     assert priority_response.status_code == 200
@@ -209,11 +258,19 @@ async def test_delete_task(client: AsyncClient):
     assert status_response.status_code == 200
 
     task_create_response = await client.post("/tasks", json=TEST_TASK_DATA)
+    assert task_create_response.status_code == 401
+    task_create_response = await client.post(
+        "/tasks", headers=headers, json=TEST_TASK_DATA
+    )
     assert task_create_response.status_code == 200
     task_data = task_create_response.json()
 
-    task_delete_response = await client.delete(f"tasks/{task_data['id']}")
+    task_delete_response = await client.delete(
+        f"tasks/{task_data['id']}", headers=headers
+    )
     assert task_delete_response.status_code == 200
 
-    task_delete_response = await client.delete(f"tasks/{task_data['id']}")
+    task_delete_response = await client.delete(
+        f"tasks/{task_data['id']}", headers=headers
+    )
     assert task_delete_response.status_code == 404
